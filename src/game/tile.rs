9b3133cd0cf34b3d3a0246ae88a95b532306fba8
cezarhg123 <cezarhg.89.popo9@gmail.com>
2022-05-23 15:46:17 +0100

    added tiles(drawing works too)

--- src/game/tile.rs
+++ src/game/tile.rs
@@ -1,11 +1,53 @@
-enum Entity {
-    None,
+use glium::backend::Facade;
+
+use crate::drawable::Drawable;
+
+#[derive(Clone, Copy)]
+pub enum Entity {
+    NoEntity,
     Apple,
     Snake
 }
 
+#[derive(Clone, Copy)]
 pub struct Tile {
     x: i32,
     y: i32,
     entity: Entity
+}
+
+impl Tile {
+    pub fn new(x: i32, y: i32, entity: Entity) -> Tile {
+        Tile {
+            x,
+            y,
+            entity
+        }
+    }
+}
+
+pub fn draw_tiles(display: &dyn Facade, tiles: &Vec<Vec<Tile>>) -> Vec<Drawable> {
+    let mut drawables: Vec<Drawable> = Vec::new();
+
+    #[allow(non_snake_case)]
+    for tArray in tiles.iter() {
+        for tile in tArray.iter() {
+            let posX = tile.x * 80;
+            let posY = tile.y * 80;
+
+            match &tile.entity {
+                Entity::NoEntity => {
+                    drawables.push(Drawable::new(display, (posX, posY), (0.2, 0.2, 0.2)));
+                },
+                Entity::Apple => {
+                    drawables.push(Drawable::new(display, (posX, posY), (1.0, 0.0, 0.0)));
+                },
+                Entity::Snake => {
+                    drawables.push(Drawable::new(display, (posX, posY), (0.0, 1.0, 0.0)));
+                }
+            }
+        }
+    };
+
+    drawables
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use game::tile::{Tile, draw_tiles, Entity};
 use glium::glutin::{self, dpi::{Size, PhysicalSize}};
 use glium::Surface;
 
@@ -22,6 +23,16 @@ fn main() {
     let fragment_shader_src = utils::read_file("shaders/default.frag");
     let shader_program = glium::Program::from_source(&display, &vertex_shader_src, &fragment_shader_src, None).unwrap();
 
+    let mut tiles:Vec<Vec<Tile>> = Vec::new();
+
+    for x in 0..10 {
+        let mut temp_vec: Vec<Tile> = Vec::new();
+        for y in 0..10 {
+            temp_vec.push(Tile::new(x, y, Entity::NoEntity));
+        }
+        tiles.push(temp_vec);
+    }
+
     let mut prev_time = std::time::Instant::now();
     //main loop
     event_loop.run(move |ev, _, control_flow| {
@@ -45,6 +56,10 @@ fn main() {
             prev_time = std::time::Instant::now();
             let mut target = display.draw();
             target.clear_color(0.0, 0.0, 0.0, 1.0);
+            let tile_drawables = draw_tiles(&display, &tiles);
+            for tile_drawable in tile_drawables.iter() {
+                target.draw(tile_drawable.get_vb(), tile_drawable.get_eb(), &shader_program, &glium::uniforms::EmptyUniforms, &Default::default()).unwrap();
+            }
             target.finish().unwrap();
         }
     });//main loop
