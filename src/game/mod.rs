cf60eb8684ea8b9245e6bfecb9be884ac77e3225
cezarhg123 <cezarhg.89.popo9@gmail.com>
2022-05-28 11:12:01 +0100

    finised game

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,3 +1,7 @@
+use rand::Rng;
+
+use crate::utils::{has_movement, get_movement_index};
+
 use self::tile::{Tile, Entity};
 
 pub mod tile;
@@ -44,7 +48,7 @@ pub struct Game {
     pub crnt_direction: Direction,
     pub tail: (i32, i32, Direction),
     pub head: (i32, i32, Direction),
-    movements: Vec<Direction>
+    movements: Vec<(i32, i32, Direction)>
 }
 
 impl Game {
@@ -81,28 +85,28 @@ impl Game {
             Input::Up => {
                 if self.crnt_direction != Direction::South {
                     self.crnt_direction = Direction::North;
-                    self.movements.push(Direction::North);
+                    self.movements.push((self.head.0, self.head.1, Direction::North));
                 }
                 *input = Input::NoInput;
             },
             Input::Down => {
                 if self.crnt_direction != Direction::North {
                     self.crnt_direction = Direction::South;
-                    self.movements.push(Direction::South);
+                    self.movements.push((self.head.0, self.head.1, Direction::South));
                 }
                 *input = Input::NoInput;
             },
             Input::Left => {
                 if self.crnt_direction != Direction::East {
                     self.crnt_direction = Direction::West;
-                    self.movements.push(Direction::West);
+                    self.movements.push((self.head.0, self.head.1, Direction::West));
                 }
                 *input = Input::NoInput;
             },
             Input::Right => {
                 if self.crnt_direction != Direction::West {
                     self.crnt_direction = Direction::East;
-                    self.movements.push(Direction::East);
+                    self.movements.push((self.head.0, self.head.1, Direction::East));
                 }
                 *input = Input::NoInput;
             },
@@ -111,6 +115,8 @@ impl Game {
     }
 
     pub fn tick(&mut self) {
+        let mut ate_apple: bool = false;
+
         self.head.0 += self.crnt_direction.value().0;
         self.head.1 += self.crnt_direction.value().1;
         self.head.2 = self.crnt_direction;
@@ -118,26 +124,43 @@ impl Game {
         {   //HEAD MECHANICS
             let x: &usize = &self.head.0.try_into().unwrap();
             let y: &usize = &self.head.1.try_into().unwrap();
+
+            if self.tiles[*x][*y].entity == Entity::Apple {
+                ate_apple = true;
+
+                loop {
+                    let rand_x: usize = rand::thread_rng().gen_range(0..10);
+                    let rand_y: usize = rand::thread_rng().gen_range(0..10);
+                    if self.tiles[rand_x][rand_y].entity == Entity::NoEntity {
+                        self.tiles[rand_x][rand_y].entity = Entity::Apple;
+                        break;
+                    }
+                }
+            }
+
+            if self.tiles[*x][*y].entity == Entity::Snake {
+                //feature
+                self.tiles[123][23].entity = Entity::NoEntity;
+            }
+
             self.tiles[*x][*y] = Tile::new(self.head.0, self.head.1, Entity::Snake); 
         }
 
         {   //TAIL MECHANICS
-            {
-                let x: usize = (self.tail.0 + self.tail.2.value().0).try_into().unwrap();
-                let y: usize = (self.tail.1 + self.tail.2.value().1).try_into().unwrap();
-
-                if self.tiles[x][y].entity == Entity::NoEntity {
-                    self.tail.2 = *self.movements.get(0).unwrap();
-                    self.movements.pop().unwrap();
-                }
+            if has_movement(&self.movements, &(self.tail.0, self.tail.1)) {
+                let index = get_movement_index(&self.movements, &(self.tail.0, self.tail.1)).unwrap();
+                self.tail.2 = self.movements.get(index).unwrap().2;
+                self.movements.remove(index);
             }
+            if !ate_apple {
+                self.tail.0 += self.tail.2.value().0;
+                self.tail.1 += self.tail.2.value().1;
 
-            self.tail.0 += self.tail.2.value().0;
-            self.tail.1 += self.tail.2.value().1;
+                let x: usize = (self.tail.0 + self.tail.2.opposite_value().0).try_into().unwrap();
+                let y: usize = (self.tail.1 + self.tail.2.opposite_value().1).try_into().unwrap();
 
-            let x: usize = (self.tail.0 + self.tail.2.opposite_value().0).try_into().unwrap();
-            let y: usize = (self.tail.1 + self.tail.2.opposite_value().1).try_into().unwrap();
-            self.tiles[x][y] = Tile::new(x.try_into().unwrap(), y.try_into().unwrap(), Entity::NoEntity);
+                self.tiles[x][y] = Tile::new(x.try_into().unwrap(), y.try_into().unwrap(), Entity::NoEntity);
+            }
         }
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,24 @@
+use crate::game::Direction;
+
 pub fn read_file(filepath: &str) -> String {
     let file = std::fs::read_to_string(filepath).expect("failed to read file dipshit");
     file
+}
+
+pub fn has_movement(vector: &Vec<(i32, i32, Direction)>, loc: &(i32, i32)) -> bool {
+    for x in vector {
+        if x.0 == loc.0 && x.1 == loc.1 {
+            return true;
+        }
+    };
+    false
+}
+
+pub fn get_movement_index(vector: &Vec<(i32, i32, Direction)>, loc: &(i32, i32)) -> Option<usize> {
+    for i in 0..vector.len() {
+        if vector[i].0 == loc.0 && vector[i].1 == loc.1 {
+            return Some(i);
+        }
+    };
+    None
 }
